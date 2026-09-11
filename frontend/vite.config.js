44b3a8baeceba79b41ea867bbeea4183d0ab92b8
hetmanenkov <hetmanenkov1510@gmail.com>
2026-09-11 15:34:40 +0200

    Fix multi-page build

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(process.cwd(), 'index.html'),
+                about: resolve(process.cwd(), 'about.html'),
+                catering: resolve(process.cwd(), 'catering.html'),
+                allergens: resolve(process.cwd(), 'allergens.html'),
+                admin: resolve(process.cwd(), 'admin.html'),
+                creator: resolve(process.cwd(), 'creator.html')
+            }
+        }
+    }
+})
